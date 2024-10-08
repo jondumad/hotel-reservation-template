51725f48897ec3425ee734bc72cb6f1e9605b9bc
jondumad <anferneehhh@gmail.com>
2024-10-08 08:31:37 +0700

    hotel booking templatedengan template admin bsb

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include_once('header.php'); ?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Dashboard Admin</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+
+<?php include_once('footer.php'); ?>
