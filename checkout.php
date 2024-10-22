e49276390df51d64c9c45208b2db1c700669fcf3
jondumad <anferneehhh@gmail.com>
2024-10-22 10:55:46 +0700

    Adding new sections

---
+++ checkout.php
@@ -0,0 +1,13 @@
+<?php include_once('header.php'); ?>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <h2>Check-In</h2>
+        </div>
+        
+    </div>
+</section>
+
+
+<?php include_once('footer.php'); ?>
