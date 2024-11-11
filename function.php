adf367dcf8d74b914906483b21d4434e4caf5e77
jondumad <anferneehhh@gmail.com>
2024-11-11 07:51:38 +0700

    Edit Reservation

--- function.php
+++ function.php
@@ -32,4 +32,40 @@ function tambah_reservasi($data)
     mysqli_query($koneksi, $query);
     return mysqli_affected_rows($koneksi);
 }
+
+function edit_reservasi($data)
+{
+    global $koneksi;
+
+    $id_reservasi = htmlspecialchars($data["id_reservasi"]);
+    $id_kamar = htmlspecialchars($data["id_kamar"]);
+    $id_tamu = htmlspecialchars($data["id_tamu"]);
+    $tgl_reservasi = htmlspecialchars($data["tgl_reservasi"]);
+    $jam_reservasi = htmlspecialchars($data["jam_reservasi"]);
+    $tgl_checkin = htmlspecialchars($data["tgl_checkin"]);
+    $tgl_checkout = htmlspecialchars($data["tgl_checkout"]);
+    $status = htmlspecialchars($data["status"]);
+
+    $query = "UPDATE `reservations` SET
+                `id_kamar` = '$id_kamar',
+                `id_tamu` = '$id_tamu',
+                `tgl_reservasi` = '$tgl_reservasi',
+                `jam_reservasi` = '$jam_reservasi',
+                `tgl_checkin` = '$tgl_checkin',
+                `tgl_checkout` = '$tgl_checkout',
+                `status` = '$status'
+                WHERE `id_reservasi` = '$id_reservasi'";
+
+    mysqli_query($koneksi, $query);
+    return mysqli_affected_rows($koneksi);
+}
+
+function delete_reservasi($id_reservasi)
+{
+    global $koneksi;
+    $query = "DELETE FROM `reservations` WHERE `id_reservasi` = '$id_reservasi'";
+    mysqli_query($koneksi, $query);
+    return mysqli_affected_rows($koneksi);
+} 
+
 ?>
